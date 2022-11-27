31e4cf0ad50b57ff11535330ac26baf78ec6cabc
ICodeMyOwnLife <icodemyownlife@gmail.com>
2022-11-27 16:36:01 +0700

    Update builds

--- packages/ts/src/index.ts
+++ packages/ts/src/index.ts
@@ -1,6 +1,6 @@
 import { Config } from 'jest';
 
-const config: Config = {
+export const config: Config = {
   moduleDirectories: ['node_modules', 'src'],
   testMatch: ['**/test/**/*.[jt]s?(x)'],
   transform: { '\\.jsx?|tsx?|cts|mts$': '@codecb/babel-jest' },
