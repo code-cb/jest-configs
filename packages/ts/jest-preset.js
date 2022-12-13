c4af0b5c6a719e9ecbd7ec2684b03028de697596
ICodeMyOwnLife <icodemyownlife@gmail.com>
2022-12-13 15:41:14 +0700

    Update

---
+++ packages/ts/jest-preset.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.config = void 0;
+exports.config = {
+    moduleDirectories: ['node_modules', 'src'],
+    testMatch: ['**/test/**/*.[jt]s?(x)'],
+    transform: { '\\.jsx?|tsx?|cts|mts$': '@codecb/babel-jest' },
+};
+exports.default = exports.config;
