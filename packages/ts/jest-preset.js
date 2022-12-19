83f0ff1a926b3ee954368b811a7515642c7c49b9
ICodeMyOwnLife <icodemyownlife@gmail.com>
2022-12-19 15:18:26 +0700

    Update

--- packages/ts/jest-preset.js
+++ packages/ts/jest-preset.js
@@ -4,6 +4,7 @@ exports.config = void 0;
 exports.config = {
     moduleDirectories: ['node_modules', 'src'],
     testMatch: ['**/test/**/*.[jt]s?(x)'],
+    testPathIgnorePatterns: ['/_\\w+.\\w+$'],
     transform: { '\\.jsx?|tsx?|cts|mts$': '@codecb/babel-jest' },
 };
 exports.default = exports.config;
--- packages/ts/jest-preset.ts
+++ packages/ts/jest-preset.ts
@@ -3,6 +3,7 @@ import { Config } from 'jest';
 export const config: Config = {
   moduleDirectories: ['node_modules', 'src'],
   testMatch: ['**/test/**/*.[jt]s?(x)'],
+  testPathIgnorePatterns: ['/_\\w+.\\w+$'],
   transform: { '\\.jsx?|tsx?|cts|mts$': '@codecb/babel-jest' },
 };
 
